127ad9c3c5b3894a128962fa9b733ddd233f0f83
Daniel Ajenifuja <335984019@educ.dpcdsb.org>
2023-02-13 21:14:19 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region enters word
+            Console.WriteLine("Enter a word!");
+            string word = Console.ReadLine();
+            #endregion
+            int p = word.Length;
+            bool ispalindrome = true;
+            // creates a loop and cuts the word halfway to only check the first letters to see if = to last
+            for (int i = 0; i < p / 2; i++)
+            {  
+                // if not = then false
+                if (word[i] != word[p - i - 1])
+                {
+                    ispalindrome = false;
+                }
+            }
+            if (ispalindrome)
+            {
+                Console.WriteLine(word + " is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine (word + " not a palindrome");
+            }
+        }
+        }
+    }
+
